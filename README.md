12b10fff4215495461b179a0ce0b1bae5ed7be25
sergio-20 <nubesgladius20@gmail.com>
2019-06-06 09:31:44 -0700

    Database grew larger. README.md file added. You can try it online now.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Learn JLPT N3 Vocab by typing out the meaning of a word while reading it.
+
+### How To Use:
+
+### Read the word and meaning, then type the meaning into the text field. After doing so, click submit or press the enter/return key.
+___
+
+# Try Online
+
+https://sergio-20.github.io/n3-vocab-trainer/
--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 let DB_Words =
 [
-  "ainiku", "akiraka", "akushu"
+  "ainiku", "akiraka", "akushu", "akeru", "azukeru", "ase", "ataeru", "atari", "ataru", "atsukau", "ato",
+  "abura", "ayamari", "ara", "arata", "arayuru", "arawasu", "araware", "aru", "aruiwa", "awa", "aware",
+  "an", "anki", "antei", "annai", "annani", "i", "iin", "yuu", "igai", "ikioi", "ikenai", "ishi", "iji",
+  "ishiki", "ijou", "izumi", "izure", "izen", "ita", "idai", "idaku", "itadaku", "itaru", "ichi", "ichiji",
+  "ichidoni"
 ];
 
 let DB_Def =
 [
-  "unfortunately", "obvious", "handshake"
+  "unfortunately", "obvious", "handshake", "to dawn", "to entrust", "sweat", "to give", "nearby", "to hit/strike", "to handle/deal with", "trace;mark;scar",
+  "oil", "error", "defect;blemish", "new", "all;every", "to reveal", "embodiment", "a certain...", "possibly", "bubble;foam", "helpless;sorrow;grief",
+  "rough draft", "memorization", "stability", "guidance", "to that extent", "stomach", "committee member", "to say", "unexpected",
+  "force;vigour;energy", "must not", "doctor;intention;volition", "preservation", "consciousness", "strange", "spring;fountain", "where;which;who",
+  "previous", "board;plank", "greatness", "to embrace/hug", "to receive", "to arrive/reach", "market;location", "for a time", "all at once"
 ];
 
 function assignVocabClick()
