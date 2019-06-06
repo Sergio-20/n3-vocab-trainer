28091a48766b0c2d8f4180a537560bfe927a1a74
sergio-20 <nubesgladius20@gmail.com>
2019-06-06 08:59:52 -0700

    Basic working version of the app added.

--- index.js
+++ index.js
@@ -8,10 +8,36 @@ let DB_Def =
   "unfortunately", "obvious", "handshake"
 ];
 
-function assignVocab()
+function assignVocabClick()
 {
   let randInt = Math.floor( Math.random() * DB_Words.length );
   let word = document.getElementById("word").innerHTML = DB_Words[randInt].toString();
   let def = document.getElementById("definition").innerHTML = DB_Def[randInt].toString();
-  console.log("Word: " + word + " Def: " + def);
+
+  let submit = document.getElementById("input-val").value = "";
+
+}
+
+function assignVocabOnLoad()
+{
+  let randInt = Math.floor( Math.random() * DB_Words.length );
+  let word = document.getElementById("word").innerHTML = DB_Words[randInt].toString();
+  let def = document.getElementById("definition").innerHTML = DB_Def[randInt].toString();
+}
+
+function assignVocab(e)
+{
+
+  let key = e.keyCode;
+
+  if( key == 13 )
+  {
+    let randInt = Math.floor( Math.random() * DB_Words.length );
+    let word = document.getElementById("word").innerHTML = DB_Words[randInt].toString();
+    let def = document.getElementById("definition").innerHTML = DB_Def[randInt].toString();
+
+    let submit = document.getElementById("input-val").value = "";
+
+  }
+
 }
