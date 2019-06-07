709269cd49fdb2e582c5fe614064e6aa854bca31
sergio-20 <nubesgladius20@gmail.com>
2019-06-07 11:54:09 -0700

    Database expanded to page 20 of 93.

--- index.js
+++ index.js
@@ -10,7 +10,14 @@ let DB_Words =
   "eien", "eikyuu", "eikyou", "eigyou", "eisei", "eiyou", "esa", "eru", "uru", "enki", "engi", "enjou", "enzetsu",
   "ensou", "enryou", "oi", "oitsuku", "ouji", "oujiru", "oudan", "ooini", "oou", "ooya", "oka", "oki", "oku", "okoru",
   "osanai", "osameru", "oshaberi", "osen", "otagai", "otoru", "onaka", "obi", "oboreru", "omoni", "oyosu", "oyobosu",
-  "oru", "orosu", "on", "ondan", "ka", "gai", "kaiga", ""
+  "oru", "orosu", "on", "ondan", "ka", "gai", "kaiga", "kaikei", "kaiketsu", "gaikou", "kaishi", "kaishaku", "gaishutsu",
+  "kaizen", "kaiteki", "kaifuku", "gaka", "kakaeru", "kakaku", "kagayaku", "kakari", "kagiru", "kagu", "gaku", "kakugo",
+  "kakujitsu", "gakusha", "gakushu", "kakudai", "kakunin", "kagen", "kago", "kakomu", "kasai", "kashi", "kaji",
+  "kashikoi", "kashu", "kasegu", "kataru", "kachi", "gakkari", "kakki", "katsudou", "katsuyou", "kanarazushimo",
+  "kanemochi", "kabu", "kaburu", "gaman", "kaminari", "kamoku", "gara", "karu", "kawaisou", "kan", "kankaku",
+  "kankyaku", "kankyou", "kangei", "kankou", "kansatsu", "kanja", "kanjou", "kanshin", "kansuru", "kansei",
+  "kanzen", "kandou", "kanryou", "kanren", "giin", "kikai", "gikai", "kikan", "kigyou", "kiku",
+  "kigen", "kikou", "kishi", "kiji", "gishi", "kisha", "kitai", "kitaku", "kichou", "gichou"
 ];
 
 let DB_Def =
@@ -29,7 +36,15 @@ let DB_Def =
   "prince", "to respond", "crossing", "greatly", "to conceal", "rich family", "hill", "open sea", "interior", "to occur", "childish",
   "to dedicate/supply", "idle talk", "pollution", "each other", "to become inferior to", "stomach", "band/sash", "to drown", "primarily",
   "approximately", "to extert", "to be for animate objects", "to get lower", "favor", "warmth", "acceptable", "injury;damage;harm", "a painting",
-  ""
+  "finance", "solution", "diplomacy", "commencement", "explanation", "going out", "improvement", "pleasant", "recovery", "painter",
+  "to carry in arms", "price", "to shine", "person in charge", "to restrict", "furniture", "forehead", "preparedness", "certainty",
+  "scholar", "study", "enlargement", "confirmation", "degree;extent", "basket;cage", "to surround", "fire", "candy", "housework",
+  "wise", "singer", "to earn income", "to talk", "merit", "to feel disappointed", "energy", "action", "practical use", "not necessarily",
+  "rich man", "a stock/share", "to suffer", "perseverance", "thunder", "school subject/course", "handgrip", "to cut/harvest",
+  "pitiable", "can/tin;perception", "sensation", "audience", "environment", "welcome;reception", "sightseeing", "observation",
+  "a patient", "calculation", "Well done!", "to be related", "completion", "completeness", "deep emotion", "completion;conclusion",
+  "revelance", "member of the Diet", "machine", "Diet;congress", "term;organ", "corporation", "to be effective", "humor;temper",
+  "climate", "coast/shore", "birthplace;article", "engineer", "reporter", "expectation", "returning home", "precious;valuable", "chairman"
 ];
 
 function assignVocabClick()
